a3e2d4b5c9888968febf3bc4598089a663e1beb4
Wataru TOYA <watrty@gmail.com>
2014-05-16 03:08:26 +0900

    完成

---
+++ cpanfile
@@ -0,0 +1,6 @@
+requires 'Furl';
+requires 'WWW::Mechanize';
+requires 'WWW::Mechanize::AutoPager';
+requires 'Web::Query';
+requires 'Path::Tiny';
+requires 'DateTime::Format::Strptime';
--- twipple.pl
+++ twipple.pl
@@ -2,25 +2,19 @@
 use strict;
 use warnings;
 
-use 5.16.0;
-#use Perl6::Say;
-use YAML;
-
-use Path::Class;
+use Path::Tiny;
 use Furl;
 use WWW::Mechanize;
 use WWW::Mechanize::AutoPager;
-use HTML::TreeBuilder::XPath;
-use HTML::Selector::XPath qw(selector_to_xpath);
+use Web::Query;
+use DateTime::Format::Strptime;
 use Log::Minimal;
-$Log::Minimal::COLOR = 1;
-$Log::Minimal::AUTODUMP = 1;
 
 my $screen_name = shift or die "usage: perl $0 twipleid";
 
 my $f = Furl->new;
 my $base = 'http://p.twpl.jp/show/orig/';
-my $dir = dir('twipple', $screen_name);
+my $dir = path('twipple_photo', $screen_name);
 $dir->mkpath unless -d $dir;
 
 my $mech = WWW::Mechanize->new;
@@ -30,16 +24,23 @@ $mech->autopager->add_site(
     pageElement => 'id("mypage")',
 );
 
-my $xpath = selector_to_xpath('div.photoList');
+my $fmt = '%b %d, %Y at %I:%M %p %Z';
+my $strp = DateTime::Format::Strptime->new(
+    pattern   => $fmt,
+    locale    => 'en_US',
+    time_zone => 'Asia/Tokyo',
+    on_error  => 'croak',
+);
 
 my $res = $mech->get("http://p.twipple.jp/user/$screen_name/detail");
 store($res);
 
 while (my $link = $mech->next_link()) {
+    sleep 5;
     my ($n) = ($link =~ m!/(\d+)!);
     say $link;
     my $res = $mech->get($link);
-#    store($res, $n);
+    store($res, $n);
 }
 
 sub store {
@@ -47,27 +48,29 @@ sub store {
 
     $n //= 1;
 
-    my $subdir = $dir;#->subdir($n);
-    $subdir->mkpath unless -d $subdir;
-
     my $content = $res->content;
-    my $tree = HTML::TreeBuilder::XPath->new;
-    $tree->parse($content);
-    $tree->eof;
-    my @nodes = $tree->findnodes($xpath);
 
-    for my $tr (@nodes) {
-        my ($id) = ($tr->attr('id') =~ /pic_(\w+)/);
+    wq($content)
+        ->find('.photoList')
+        ->each(sub {
+            my $i = shift;
+            my ($photo_id) = ($_->attr('id') =~ /\Apic_thumb_(\w+)\z/);
 
-        my $uri = $base . $id;
+            my $date = $_->find('.photoDetail > span:nth-child(2)')->text();
+               $date =~ s/\A\s*|\s*\z//g;
+            my $dt = $strp->parse_datetime($date);
 
-        my $fres = $f->get($uri);
+            my $uri = $base . $photo_id;
 
-        if ($fres->is_success) {
-            my $fh = $subdir->file("$id.jpg")->openw;
-            binmode $fh;
-            print $fh $fres->body;
-            close $fh;
-        }
-    }
+            infof '%s %s', $dt->datetime, $uri;
+
+            my $fres = $f->get($uri);
+
+            if ($fres->is_success) {
+                my $fh = $dir->child($dt->ymd('-') . '-' . $dt->hms('-') . '_' . $photo_id . '.jpg')->openw;
+                binmode $fh;
+                print $fh $fres->body;
+                close $fh;
+            }
+        });
 }
